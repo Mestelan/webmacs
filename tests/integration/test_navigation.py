62d7c96faa5c5729e59dbc347cc6ce4b5d0c5df1
Julien Pagès <j.parkouss@gmail.com>
2018-08-19 17:00:43 +0200

    Add two more follow hinting tests

--- tests/integration/test_navigation.py
+++ tests/integration/test_navigation.py
@@ -26,3 +26,55 @@ def test_navigate_new_buffer(session):
         return sorted_buff_titles() == ["link one", "navigation"]
 
     assert session.wait_until(new_buffer_loaded, 5.0)
+
+
+def test_navigate_follow_hinting(session):
+    session.load_page("navigation")
+
+    assert session.buffer.title() == "navigation"
+
+    session.wkeyclicks("f")
+
+    # wait for the first element to be selected
+    session.check_js_follow_element_selected(
+        "document.getElementById('link1')"
+    )
+
+    session.keyclicks("two")
+
+    # now wait for the second element to be selected
+    session.check_js_follow_element_selected(
+        "document.getElementById('link2')"
+    )
+
+    session.wkeyclicks("Enter")
+
+    assert session.wait_until(
+        lambda: session.buffer.title() == "link two"
+    )
+
+
+def test_navigate_follow_hinting_numbers(session):
+    session.load_page("navigation")
+
+    assert session.buffer.title() == "navigation"
+
+    session.wkeyclicks("f")
+
+    # wait for the first element to be selected
+    session.check_js_follow_element_selected(
+        "document.getElementById('link1')"
+    )
+
+    session.keyclicks("2")
+
+    # now wait for the second element to be selected
+    session.check_js_follow_element_selected(
+        "document.getElementById('link2')"
+    )
+
+    session.wkeyclicks("Enter")
+
+    assert session.wait_until(
+        lambda: session.buffer.title() == "link two"
+    )
